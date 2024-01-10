745b785805553244b37ea191315f35bd26c443bd
Evgeniia Shalanova <sep@iglootest.ru>
2024-01-10 22:26:35 +0300

    Ex3

--- main.py
+++ main.py
@@ -3,3 +3,6 @@ response = requests.get("https://playground.learnqa.ru/api/hello")
 print(response.text)
 
 print("Hello from Jane")
+
+ex4 = requests.get("https://playground.learnqa.ru/api/get_text")
+print(ex4.text)
